e347716b30c3bc166aec9de9e414cce5486b9a0f
omarmhmmd <omar.mhmmd@gmail.com>
2018-10-05 11:41:03 -0700

    search_filter 004 overlay

--- js/search_filter.js
+++ js/search_filter.js
@@ -1,32 +1,73 @@
+/**** SHOW MENU ****/
+var show = false;
+function showMenu() {
+  var x = document.getElementById("menu");
+  if (show == false) {
+    x.style.display = "block";
+    document.getElementById('searchButton').src='svg/X.svg';
+    document.getElementById('overlay').style.zIndex = "1";
+    show = true;
+  }
+  else if (show == true ){
+    x.style.display = "none";
+    document.getElementById('searchButton').src='svg/SearchGlass.svg';
+    document.getElementById('overlay').style.zIndex = "-1";
+    show = false;
+  }
+
+  /**** OVERLAY ****/
+
+
+}
+
+/**** SHOW ACTIVE STATE OF FILTERS ****/
+// Get the container element
+var btnContainer = document.getElementById("filters");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
+/**** SHOW ACTIVE STATE OF FILTERS ****/
+
 /**** SHOW TAGS ****/
-function ShowTags(filter) {
+function showTags(filter) {
   var filterTags = "";
   var filterTags = filter;
   var filterTags = filterTags + "Tags";
-  console.log(filter);
 
   var tags = document.getElementById(filterTags);
-  var allTags = document.getElementsByClassName("Tags");
 
-  var y;
   if (filterTags == "decadeTags") {
+    document.getElementById("decadeTags").style.display = "flex";
     document.getElementById("formatTags").style.display = "none";
     document.getElementById("techTags").style.display = "none";
   } else if (filterTags == "formatTags") {
+    document.getElementById("formatTags").style.display = "flex";
     document.getElementById("decadeTags").style.display = "none";
     document.getElementById("techTags").style.display = "none";
-  }
-  else if (filterTags == "techTags") {
+  } else if (filterTags == "techTags") {
+    document.getElementById("techTags").style.display = "flex";
     document.getElementById("formatTags").style.display = "none";
     document.getElementById("decadeTags").style.display = "none";
   }
 
-  var colorText = document.getElementById(filter);
-  if (tags.style.display === "flex") {
-    tags.style.display = "none";
-  }
-  else {
-    tags.style.display = "flex";
-  }
+  // if (tags.style.display === "flex") {
+  //   tags.style.display = "none";
+  // }
+  // else {
+  //   tags.style.display = "flex";
+  // }
 }
 /**** END SHOW TAGS ****/
--- js/search_filter001.js
+++ js/search_filter001.js
@@ -9,51 +9,51 @@
 //   });
 // });
 //
-var created = false;
-
-function ShowTags(filter) {
-  created = true;
-
-  if (created == true) {
-    $(".formatTags p").html("");
-  }
-
-  var filterTags = "";
-  var filterTags = filter;
-  var filterTags = filterTags + "Tags";
-
-  var tags = document.getElementById(filterTags);
-  var colorText = document.getElementById(filter);
-  if (tags.style.display === "flex") {
-    tags.style.display = "none";
-  }
-  else {
-    tags.style.display = "flex";
-  }
-
-  console.log(filter)
-  if (filter == "format") {
-    var data = [
-      {'name':'Book'},
-      {'name':'Booklet'},
-      {'name':'Brochure'},
-      {'name':'Business Card'},
-      {'name':'Clipping'},
-      {'name':'Greeting Card'},
-      {'name':'Label'},
-      {'name':'Letter'},
-      {'name':'Magazine'},
-      {'name':'Poster'},
-      {'name':'Print'},
-      {'name':'Sketch'},
-      {'name':'Textile'},
-      {'name':'Type Specimen'}
-    ];
-  }
-
-
-
-  var transform = {'<>':'button','text':'${name}'};
-  $('.formatTags p').json2html(data,transform);
-
-}
+// var created = false;
+//
+// function ShowTags(filter) {
+//   created = true;
+//
+//   if (created == true) {
+//     $(".formatTags p").html("");
+//   }
+//
+//   var filterTags = "";
+//   var filterTags = filter;
+//   var filterTags = filterTags + "Tags";
+//
+//   var tags = document.getElementById(filterTags);
+//   var colorText = document.getElementById(filter);
+//   if (tags.style.display === "flex") {
+//     tags.style.display = "none";
+//   }
+//   else {
+//     tags.style.display = "flex";
+//   }
+//
+//   console.log(filter)
+//   if (filter == "format") {
+//     var data = [
+//       {'name':'Book'},
+//       {'name':'Booklet'},
+//       {'name':'Brochure'},
+//       {'name':'Business Card'},
+//       {'name':'Clipping'},
+//       {'name':'Greeting Card'},
+//       {'name':'Label'},
+//       {'name':'Letter'},
+//       {'name':'Magazine'},
+//       {'name':'Poster'},
+//       {'name':'Print'},
+//       {'name':'Sketch'},
+//       {'name':'Textile'},
+//       {'name':'Type Specimen'}
+//     ];
+//   }
+//
+//
+//
+//   var transform = {'<>':'button','text':'${name}'};
+//   $('.formatTags p').json2html(data,transform);
+//
+// }
