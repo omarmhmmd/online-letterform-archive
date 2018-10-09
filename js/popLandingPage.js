83f1736a13215b5107505fcd2331367abb1f3aad
omarmhmmd <omar.mhmmd@gmail.com>
2018-10-08 18:41:04 -0700

    search_filter 007 grid with JSON call 001

---
+++ js/popLandingPage.js
@@ -0,0 +1,7 @@
+var arrItems = []; // THE ARRAY TO STORE JSON ITEMS.
+$.getJSON("data/results.json", function(json) {
+  $.each(json.resultsPage, function(index, element) {
+    arrItems.push(element.imageURL);
+    $('.column').append('<img src="' + element.imageURL+ '" style="width:100%">')
+  });
+});
