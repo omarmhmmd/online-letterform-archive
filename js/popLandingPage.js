df619bd358f4cc8e4a4a26788ae62a2ecc67db38
omarmhmmd <omar.mhmmd@gmail.com>
2018-10-09 13:07:31 -0700

    added entrypage json

--- js/popLandingPage.js
+++ js/popLandingPage.js
@@ -1,7 +1,21 @@
-var arrItems = []; // THE ARRAY TO STORE JSON ITEMS.
-$.getJSON("../data/images.json", function(json) {
-  $.each(json.resultsPage, function(index, element) {
-    arrItems.push(element.imageURL);
-    $('.column').append('<img src="' + element.imageURL+ '" style="width:100%">')
+var arrItems001 = [];
+var arrItems002 = [];
+var arrItems003 = [];
+var arrItems004 = [];
+
+$.getJSON("datablobs/entrypage.json", function(json) {
+  $.each(json.entryPageItems, function(index, element) {
+    if (index > 0 && index < 8) {
+      $('#column001').append('<img src="' + element.entryContent.imageURL + '" style="width:100%">');
+    }
+    else if (index > 8 && index < 16) {
+      $('#column002').append('<img src="' + element.entryContent.imageURL + '" style="width:100%">');
+    }
+    else if (index > 16 && index < 24) {
+      $('#column003').append('<img src="' + element.entryContent.imageURL + '" style="width:100%">');
+    }
+    else if (index > 24 && index < 32) {
+      $('#column004').append('<img src="' + element.entryContent.imageURL + '" style="width:100%">');
+    }
   });
 });
