6030b32161e23aa0b7da77d545aa95b72ba9c317
omarmhmmd <omar.mhmmd@gmail.com>
2018-10-09 14:53:27 -0700

    added entrypage json 001

--- js/popLandingPage.js
+++ js/popLandingPage.js
@@ -1,21 +1,20 @@
-var arrItems001 = [];
-var arrItems002 = [];
-var arrItems003 = [];
-var arrItems004 = [];
-
 $.getJSON("datablobs/entrypage.json", function(json) {
   $.each(json.entryPageItems, function(index, element) {
-    if (index > 0 && index < 8) {
-      $('#column001').append('<img src="' + element.entryContent.imageURL + '" style="width:100%">');
-    }
+    if (index >= 0 && index < 8) {
+      $('#column001').append(
+        '<div class = "hoverLay"> <a target="_blank"href="https://www.google.com/"> <div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' +element.entryContent.title + '</figcaption> </div> </a> ');
+      }
     else if (index > 8 && index < 16) {
-      $('#column002').append('<img src="' + element.entryContent.imageURL + '" style="width:100%">');
-    }
+      $('#column002').append(
+        '<div class = "hoverLay"> <div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' +element.entryContent.title + '</figcaption> </div>');
+      }
     else if (index > 16 && index < 24) {
-      $('#column003').append('<img src="' + element.entryContent.imageURL + '" style="width:100%">');
-    }
+      $('#column003').append(
+        '<div class = "hoverLay"> <div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' +element.entryContent.title + '</figcaption> </div>');
+      }
     else if (index > 24 && index < 32) {
-      $('#column004').append('<img src="' + element.entryContent.imageURL + '" style="width:100%">');
-    }
+      $('#column004').append(
+        '<div class = "hoverLay"> <div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' +element.entryContent.title + '</figcaption> </div>');
+      }
   });
 });
