c1553e1f832b5d692e543872d7d251366f02dbf6
omarmhmmd <omar.mhmmd@gmail.com>
2018-10-08 18:51:41 -0700

    getting github to recognize json

--- js/popLandingPage.js
+++ js/popLandingPage.js
@@ -1,5 +1,5 @@
 var arrItems = []; // THE ARRAY TO STORE JSON ITEMS.
-$.getJSON("../data/results.json", function(json) {
+$.getJSON("../data/images.json", function(json) {
   $.each(json.resultsPage, function(index, element) {
     arrItems.push(element.imageURL);
     $('.column').append('<img src="' + element.imageURL+ '" style="width:100%">')
