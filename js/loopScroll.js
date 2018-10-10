b13033ab59f5e343e1db6da9656344272c2a7d39
omarmhmmd <omar.mhmmd@gmail.com>
2018-10-10 15:10:55 -0700

    loop on mobile

--- js/loopScroll.js
+++ js/loopScroll.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
 
   },
 
+  a.sizes = {},
+
 	a.ui = {
 		body: $('body'),
 		win: $(window)
@@ -16,7 +18,11 @@ $(document).ready(function(){
 		init: function(){
       if(a.settings.screenType !== 'touch'){
 				this.clonePage();
+        console.log("desktop");
 			}
+      else if(a.settings.screenType == 'touch'){
+        console.log('mobile');
+      }
 		},
 
 		clonePage: function(){
@@ -36,6 +42,26 @@ $(document).ready(function(){
 		}
 	}
 
+  a.helpers = {
+
+  	init: function(){
+  		this.checkMediaQuery();
+  	},
+
+  	checkMediaQuery: function(){
+  		if(!$('.media-check').length){$('body').append('<div class="media-check"></div>')}
+  		var mediaCheck = $('.media-check').css('text-indent');
+  		if(mediaCheck === '10px'){a.sizes.media = 'mobile';}
+  		else if(mediaCheck === '20px'){a.sizes.media = 'tablet';}
+  		else if(mediaCheck === '30px'){a.sizes.media = 'desktop';}
+  		else if(mediaCheck === '40px'){a.sizes.media = 'xl';}
+  		else if(mediaCheck === '50px'){a.sizes.media = 'xxl';}
+  		else {a.sizes.media = 'unsure';}
+  		a.settings.screenType = $('html').hasClass('mobile') || $('html').hasClass('tablet') ? 'touch' : 'mouse';
+  	},
+	}
+
+	a.helpers.init();
 	a.site.init();
 });
 /**** CODE SOURCED FROM https://dow-smith.com/ ****/
