28bc91bd8debd02af07eb5592f69cb214047583b
omarmhmmd <omar.mhmmd@gmail.com>
2018-10-11 11:06:55 -0700

    results grid up - styling issues on menu filter

--- js/popLandingPage.js
+++ js/popLandingPage.js
@@ -6,15 +6,15 @@ $.getJSON("datablobs/entrypage.json", function(json) {
       }
     else if (index > 8 && index < 16) {
       $('#column002').append(
-        '<div class = "hoverLay"> <div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' +element.entryContent.title + '</figcaption> </div>');
+        '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' +element.entryContent.title + '</figcaption> </div>');
       }
     else if (index > 16 && index < 24) {
       $('#column003').append(
-        '<div class = "hoverLay"> <div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' +element.entryContent.title + '</figcaption> </div>');
+        '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' +element.entryContent.title + '</figcaption> </div>');
       }
     else if (index > 24 && index < 32) {
       $('#column004').append(
-        '<div class = "hoverLay"> <div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' +element.entryContent.title + '</figcaption> </div>');
+        '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' +element.entryContent.title + '</figcaption> </div>');
       }
   });
 });
---
+++ js/popResultsPage.js
@@ -0,0 +1,6 @@
+$.getJSON("datablobs/results.json", function(json) {
+  $.each(json.resultsPage, function(index, element) {
+      $('#resultsGrid').append(
+        '<div class = "results-item colour"> <a target="_blank"href="https://www.google.com/"> <img src="' + element.imageURL + '" style="width:100%" > <figcaption> ' + element.title + '</figcaption> </div> </a> ');
+  });
+});
