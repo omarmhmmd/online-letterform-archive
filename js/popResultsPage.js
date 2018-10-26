8948f5787c58d56998aa100e2eb7349360b4a00c
omarmhmmd <omar.mhmmd@gmail.com>
2018-10-26 08:49:15 -0700

    testing servers 001

--- js/popResultsPage.js
+++ js/popResultsPage.js
@@ -1,6 +1,6 @@
-$.getJSON("datablobs/entrypage.json", function(json) {
-  $.each(json.entryPageItems, function(index, element) {
+$.getJSON("datablobs/results.json", function(json) {
+  $.each(json.resultsPage, function(index, element) {
       $('#resultsGrid').append(
-        '<div class = "results-item colour"> <a target="_blank"href="https://www.google.com/"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.title + '</figcaption> </div> </a> ');
+        '<div class = "results-item colour"> <a target="_blank"href="https://www.google.com/"> <img src="' + element.imageURL + '" style="width:100%" > <figcaption> ' + element.title + '</figcaption> </div> </a> ');
   });
 });
