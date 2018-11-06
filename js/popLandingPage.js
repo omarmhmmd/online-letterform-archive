873445f149dee63c512c60f5df8142c84d768334
omarmhmmd <omar.mhmmd@gmail.com>
2018-11-06 13:05:18 -0800

    double span w/ search blocks in place

--- js/popLandingPage.js
+++ js/popLandingPage.js
@@ -1,4 +1,17 @@
-$.getJSON("datablobs/entrypage.json", function(json) {
+$.getJSON("datablobs/entrypage001.json", function(json) {
+
+  // POPULATE TEXT IN SEARCH BLOCKS //
+  var searchTextH1 = document.getElementsByClassName('searchText');
+  console.log(searchTextH1);
+
+  $.each(json.entryPageSearches, function(index, element) {
+    console.log(index);
+    // console.log(element.entryContent.searchText);
+    searchTextH1[index].innerHTML = element.entryContent.searchText;
+  });
+  // END POPULATE TEXT IN SEARCH BLOCKS //
+
+  // POPULATE IMAGES IN ENTRY GRID //
   $.each(json.entryPageItems, function(index, element) {
     if (index == 0) {
       $('#subColumn001').append(
@@ -12,11 +25,7 @@ $.getJSON("datablobs/entrypage.json", function(json) {
       $('#subColumn002').append(
         '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
     }
-    // else if (index >= 4 && index < 6) {
-    //   $('#subColumn003').append(
-    //     '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
-    // }
-    else if (index >= 4 && index < 5) {
+    else if (index >= 4 && index < 6) {
       $('#subColumn003').append(
         '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
     }
@@ -32,13 +41,30 @@ $.getJSON("datablobs/entrypage.json", function(json) {
       $('#subColumn006').append(
         '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
     }
-    // else if (index >= 12 && index < 14) {
-    //   $('#subColumn007').append(
-    //     '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
-    // }
-    // else if (index >= 14 && index < 16) {
-    //   $('#subColumn008').append(
-    //     '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
-    // }
+    else if (index >= 12 && index < 14) {
+      $('#subColumn007').append(
+        '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
+    }
+    else if (index >= 14 && index < 16) {
+      $('#subColumn008').append(
+        '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
+    }
+    else if (index >= 16 && index < 18) {
+      $('#subColumn009').append(
+        '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
+    }
+    else if (index >= 18 && index < 20) {
+      $('#subColumn010').append(
+        '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
+    }
+    else if (index >= 20 && index < 22) {
+      $('#subColumn011').append(
+        '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
+    }
+    else if (index >= 22 && index < 24) {
+      $('#subColumn012').append(
+        '<div class = "hoverLay"> <img src="' + element.entryContent.imageURL + '" style="width:100%" > <figcaption> ' + element.entryContent.title + '</figcaption> </div>');
+    }
   });
+  // END POPULATE IMAGES IN ENTRY GRID //
 });
